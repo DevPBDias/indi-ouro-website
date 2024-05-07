e47485ee508f275ae4624503dae8fe03893082a8
DevPBDias <devpbdias@gmail.com>
2024-05-06 23:53:34 -0300

    feat: separating about page into components

---
+++ src/components/about/FarmImage.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import whoImg from '../../../public/images/who.png'
+import Image from 'next/image'
+
+const FarmImage = () => {
+    return (
+        <picture className='w-1/2 h-full'>
+            <Image src={whoImg} alt='farm view' className='w-full h-full' />
+        </picture>
+    )
+}
+
+export default FarmImage
---
+++ src/components/about/Text.tsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+const Text = () => {
+    return (
+        <section className='w-1/2 h-full flex flex-col justify-between items-start'>
+            <h1 className='text-4xl uppercase font-bold text-[--primary-color]'>Quem somos</h1>
+            <p className='text-base'>
+                Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.
+                <br />
+                <br />
+
+                Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.
+                <br />
+                <br />
+
+                Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.
+                <br />
+                <br />
+
+                Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.</p>
+        </section>
+    )
+}
+
+export default Text
