ec54df37f2221f97edd3340e5c474ff92dfe6d61
devpbdias <devpbdias@gmail.com>
2024-06-10 12:50:40 -0300

    fix: removed admin page and components and header section

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -2,17 +2,13 @@ import React from "react";
 import Logo from "../Logo";
 import Navbar from "./Navbar";
 import IconsBar from "../IconsBar";
-import AdminBtn from "./AdminBtn";
 
 const Header = () => {
   return (
-    <header className="bg-[--primary-color] w-full h-20 px-[10%]">
-      <section className="flex flex-row justify-between items-center h-full">
-        <Logo />
-        <Navbar />
-        <IconsBar />
-        <AdminBtn />
-      </section>
+    <header className="bg-[--primary-color] w-full h-20 px-[10%] flex flex-row justify-between items-center">
+      <Logo />
+      <Navbar />
+      <IconsBar />
     </header>
   );
 };
